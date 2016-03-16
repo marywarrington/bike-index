978966621c3d280ca2f69d907508e2ec6181e38f
Michelle Brecunier and Mary Warrington <michelle.brecunier@gmail.com>
2016-03-16 09:56:22 -0700

    Add loop to display title and frame colors

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   $('.showBikes').hide();
   $('#searchBikes').click(function() {
-    var zipcode = $('#zipcode').val();
+    var apiRoot = "https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=15&";
+    var zipcode = "poximity=" + $('#zipcode').val();
+    var color = "colors=" + $('#color').val();
     $('#zipcode').val("");
+    $('#color').val("");
     $('.showBikes').show();
-    $('#testBike2').text("TEST1");
+    $.get(apiRoot + color + zipcode + '&proximity_square=16&access_token=api_key').then(function(response) {
+      for (i = 0; i < response.bikes.length; i++) {
 
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=5&proximity=97214&proximity_square=16&access_token=api_key').then(function(response) {
-      $('#testBike2').text("tests");
-      $('#testBike').text(response.bikes[0].title);
+        $('#testBike').append('<li>' + response.bikes[i].title + ", " + response.bikes[i].frame_colors.join() + '</li>');
+
+      }
 
 
 
